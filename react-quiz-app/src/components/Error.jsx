3bf0177c5d240911ef46690d8e9e22eb8fdfd3e1
Eniola Fanegan <eniolachinemerem74@gmail.com>
2024-06-11 05:27:36 -0500

    Quiz app with React
    Quiz app with React, Vite

---
+++ react-quiz-app/src/components/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className='bg-dark max-w-sm mx-auto text-center p-4 rounded-3xl'>
+        <span className='text-white font-bold'>💥 Bombastic side eye... Criminal Offensive side eye</span>
+    </div>
+  )
+}
+
+export default Error
